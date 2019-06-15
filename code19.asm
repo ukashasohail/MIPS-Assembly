2d5f79fe05de4c8ffb7458dcbf3b4a6864d1308c
ukasha <ukasha909@outlook.com>
2019-06-15 19:28:23 +0500

    code19

--- code19.asm
+++ code19.asm
@@ -1,6 +1,7 @@
 .data
 
     prompt: .asciiz "enter value of pi: "
+    zerofloat: .float 0.0
 
 .text
 
@@ -8,6 +9,22 @@
 .ent main
 
 main:
+
+    lwc1 $f4, zerofloat
+
+    #displaying message
+    li $v0, 4 
+    la $a0, prompt
+    syscall
+
+    #reading float
+    li $v0, 6 #input of float moved to #$f0
+    syscall
+
+    #display value
+    li $v0, 2
+    add.s $f12, $f0, $f4
+    syscall
 
     li $v0, 10
     syscall
