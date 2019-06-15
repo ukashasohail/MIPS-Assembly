16db994a8f92f84bdd915deb8861ffe546eba098
ukasha <ukasha909@outlook.com>
2019-06-15 17:12:56 +0500

    code15

--- code14.asm
+++ code14.asm
@@ -1,13 +1,30 @@
 .data
 
+    msg: .asciiz "hi how are you?\nim ukasha\n"
 
 .text
 
 .globl main
 .ent main
 
+#this is caller
 main:
 
-    jr $ra
+    jal displaymsg
+
+    #exit
+    li $v0, 10
+    syscall
+
+    #this is callee
+    displaymsg:
+        li $v0, 4
+        la $a0, msg
+        syscall
+
+        jr $ra
+
+    .end displaymsg
+
 
 .end main
